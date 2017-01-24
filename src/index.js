91e78c737af7bd3b70078196942af411d8bd0493
Shaun Grady <grady@shaungrady.com>
2017-01-24 14:58:31 -0800

    Restructure source into ES6 modules, reformat to Standard style

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import angular from 'angular'
+import moment from 'moment'
+
+import service from './service'
+import filter from './filter'
+import inputDirective from './directive_input'
+import inputPickerDirective from './directive_inputPicker'
+import pickerDirective from './directive_picker'
+
+export default angular
+  .module('$moment', [])
+  .provider('$moment', service)
+  .filter('momentFormat', filter)
+  .directive('input', inputDirective)
+  .directive('inputPicker', inputPickerDirective)
+  .directive('momentPicker', pickerDirective)
+  .name
